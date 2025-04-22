59d66c7710b5c748a6af8c19f0fdd4d39d828884
osvaldocs <100969798+osvaldocs@users.noreply.github.com>
2025-04-22 08:30:40 -0500

    Add files via upload

---
+++ ProyectoLibreMercado.py
@@ -0,0 +1,37 @@
+import math
+
+print("Bienvenido a Libre Mercado")
+print()
+
+
+print("Ingrese el nombre del producto:")
+producto = str(input())
+
+print("Por favor ingrese el precio unitario:")
+precio = int(input())
+s
+while precio < 1:
+    print("INGRESE UN PRECIO REAL:")
+    precio = int(input())
+
+print("Por favor ingrese la cantidad de productos:")
+cantidad = int(input())
+
+while cantidad < 1:
+    print("Ingrese una cantidad válida:")
+    cantidad = int(input())
+
+print("Por favor ingrese el porcentaje de descuento:")
+descuento = int(input())
+
+while descuento < 1 or descuento > 100:
+    print("INGRESE UN DESCUENTO RAZONABLE:")
+    descuento = int(input())
+
+
+valorTotal = precio * cantidad
+porDescuento = valorTotal * descuento /100
+valorFinal = valorTotal - porDescuento
+
+print(f"El precio total por  {cantidad}   {producto} es: {valorFinal}")
+
